27ec963092ecc97f86b1648111efdf1ef9d90595
ivan.radonjic <ivan.radonjic@activecollab.com>
2015-11-20 16:58:25 +0100

    Fix path in bootstrap

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -3,4 +3,4 @@ defined('BASE_PATH') || define('BASE_PATH', realpath(__DIR__));
 
 date_default_timezone_set('GMT');
 require dirname(__DIR__) . '/vendor/autoload.php';
-require __DIR__ . '/src/TestCase.php';
+require __DIR__ . '/src/Commands/TestCase.php';
